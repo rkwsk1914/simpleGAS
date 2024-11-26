59fc413380b684426340dd048a9868bb35573ffb
rkwsk-dsk <ryo_kawasaki@densan-s.co.jp>
2024-11-26 12:14:54 +0900

    main.tsのbuildエラーを解消

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { MY_ACCOUNT_ID } from '@/const/settings'
 
 // import { GASController } from '@/app/GASController'
 import { LineApp } from '@/app/LineApp'
 
+declare const global: any
+
 const LineApplication = new LineApp()
 // const gas = new GASController()
 
-function doPost(e: GoogleAppsScript.Events.DoPost) {
+global.doPost = (e: GoogleAppsScript.Events.DoPost) => {
   LineApplication.checkMessageAndPost(e)
 }
 
-function doPostMessages() {
+global.doPostMessages = () => {
   LineApplication.post(MY_ACCOUNT_ID ?? '', [{
     type: 'text',
     text: 'GASからLINEに送信'
